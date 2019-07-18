6f7d76bef4e79704eae0e86d2b0280c6a3a14522
- <->
2019-07-18 18:39:55 +0800

    .

--- generator.js
+++ generator.js
@@ -2,7 +2,7 @@
 module.exports = function(api){
   return api.extendPackage({
     devDependencies: {
-      '6du-livescript-loader': '^0.0.2',
+      '6du-livescript-loader': '^0.0.3',
       livescript: "^1.6.0"
     }
   });
